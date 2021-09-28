c0cb44325f948779a132745fc9ba6f27ffaefbce
Christian Lent <christian@lent.us>
2021-09-27 23:48:51 -0400

    Force format in black

--- rtpdir/rtpmodule.py
+++ rtpdir/rtpmodule.py
@@ -1,12 +1,6 @@
-def raisetopower(root:int,exponent:int)->int:
-
-
-
+def raisetopower(root: int, exponent: int) -> int:
 
     if exponent == 0:
         return 1
 
-
-
-
-    return root * raisetopower(root,exponent-1)
+    return root * raisetopower(root, exponent - 1)
